f07ab5cae3def523a202a2d80ebdeece651e600d
ManojAGowda <90241350+ManojAGowda@users.noreply.github.com>
2022-01-07 14:46:33 +0530

    Update Recipes.js

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -5,8 +5,8 @@ function Recipes({item}) {
 
   return (
     <div>
-      <h1 className='mt-2 dish-head'>Recipe</h1>
-      <div className="dish-card border mt-3">
+      <h1 className='mt-2 item-head'>Recipe</h1>
+      <div className="item-card border mt-3">
         <img src={item.image} className="card-img-top" alt={item.name} />
         <div className="card-body">
           <h3>{item.name}</h3>
